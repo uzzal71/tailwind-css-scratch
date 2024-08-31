29209de03d38d0ea2cac76757d02a5ae555832a6
uzzal71 <uzzalroy.acm@gmail.com>
2024-08-31 10:36:42 +0600

    Typography

---
+++ Section-02/04-typography/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia", "Tapestry"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      display: ["Oswald"],
+      body: ['"Open Sans"'],
+    },
+  },
+};
